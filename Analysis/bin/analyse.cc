a8473e5c073e592a563fbf84562f60aae34e51d2
defranchis <matteo.defranchis@cern.ch>
2018-02-28 16:30:35 +0100

    Revert "flat 1% for electron ES uncertainty"
    This reverts commit ed21f58cc01f033ce9db0b4a81be57d9a876e095.

--- Analysis/bin/analyse.cc
+++ Analysis/bin/analyse.cc
@@ -226,8 +226,8 @@ invokeApplication(){
 	if(elecEnsffile.EndsWith("DEFFILEWILDCARDDONTREMOVE")){
 		if(energy == "7TeV" || energy == "8TeV")
 			ana->getElecEnergySF()->setGlobal(1,0.15,0.15);
-		else if(energy == "13TeV")
-			ana->getElecEnergySF()->setGlobal(1,1.0,1.0);
+		// else if(energy == "13TeV")
+		// 	ana->getElecEnergySF()->setGlobal(1,1.0,1.0);
 
 	}
 	else{
