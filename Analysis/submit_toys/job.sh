4ce2f997fec58e6d8dd4989e51aeaf8f8b0e8f46
defranchis <matteo.defranchis@cern.ch>
2018-04-19 11:51:36 +0200

    scipts to submit toys

---
+++ Analysis/submit_toys/job.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+cd /nfs/dust/cms/user/defrancm/MassExtraction2016/Till/CMSSW_8_0_26_patch2/src/
+eval `scramv1 runtime -sh`
+cd TtZAnalysis/Analysis/toys_workdir
+nohup fitTtBarXsec -i emu_mtop_13TeV_preapp_gaussMass.txt --emu -M --noplots --onlytotal --nToys $1 --seed $2 &> log_$2.log
