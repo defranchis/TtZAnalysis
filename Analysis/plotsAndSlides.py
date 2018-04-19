8a6b525c501f6b86f9ffcbde0c9d815e0e80fc93
defranchis <matteo.defranchis@cern.ch>
2018-04-19 11:50:41 +0200

    scripts for toy experiments

---
+++ Analysis/plotsAndSlides.py
@@ -0,0 +1,47 @@
+import os
+import ROOT
+from ROOT import *
+
+inFile = 'toys.root'
+outdir = 'plots_toys'
+
+rootFile = ROOT.TFile(inFile,'r')
+
+if not os.path.exists(outdir):
+    os.makedirs(outdir)
+
+c = ROOT.TCanvas('c','c')
+
+histolist = rootFile.GetListOfKeys()
+for histokey in histolist: 
+    if histokey.GetClassName() != 'TH1F': continue
+    name = histokey.GetName()
+    histo = rootFile.FindObjectAny(name)
+    c.Clear()
+    histo.Draw()
+    outname = TString(name).ReplaceAll(' ','')
+    outname.ReplaceAll('\\','')
+    outname.ReplaceAll('/','')
+    outname.ReplaceAll('$','')
+    outname.ReplaceAll('.','_')
+    outname.ReplaceAll('(','')
+    outname.ReplaceAll(')','')
+    outname.ReplaceAll('{','')
+    outname.ReplaceAll('}','')
+    if outname.EndsWith('_constr'): histo.Rebin()
+    outfilename = TString(outdir+'/'+str(outname)+'.pdf')
+    c.SaveAs(str(outfilename),'pdf')
+
+    if outname.EndsWith('_pull'):
+        title = outname.ReplaceAll('_pull','')
+        title.ReplaceAll('_','-')
+        title.ReplaceAll('^','')
+        if '13TeV' in str(title): continue
+        print '\\begin{frame}{'+str(title)+'}'
+        print '\includegraphics[width=.5\\textwidth]{'+str(outfilename)+'}'
+        outfilename.ReplaceAll('_pull','_constr')
+        print '\includegraphics[width=.5\\textwidth]{'+str(outfilename)+'} \\\\'
+        outfilename.ReplaceAll('_constr','_contrib')
+        print '\includegraphics[width=.5\\textwidth]{'+str(outfilename)+'}'
+        print '\\end{frame}'
+        print
