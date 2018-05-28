4686d6fc54594a88ec70734912c4274d1496fa97
defranchis <matteo.defranchis@cern.ch>
2018-05-28 14:08:08 +0200

    unnecessary cosmetics

--- Analysis/submit_toys/templates/job_HTCondor.sh
+++ Analysis/submit_toys/templates/job_HTCondor.sh
@@ -9,11 +9,11 @@ nToys=$2
 config=$3
 mass_fit=$4
 
-echo "seed -1 = " $1
-echo "ntoys = " $2
-echo "config = " $3
-echo "mass fit = " $4
-
+echo "seed -1 =" $1
+echo "ntoys =" $2
+echo "config =" $3
+echo "mass fit =" $4
+echo
 
 if [ "$mass_fit" == true ]; then
     fitTtBarXsec -i $config --emu -M --noplots --onlytotal --nToys $nToys --seed $((seed+1))
