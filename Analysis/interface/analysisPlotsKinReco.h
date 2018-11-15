c279e035c0f2fac0e56f5a43023542bd2a41054e
defranchis <matteo.defranchis@cern.ch>
2018-11-15 11:02:38 +0100

    plots in bins of mtt

--- Analysis/interface/analysisPlotsKinReco.h
+++ Analysis/interface/analysisPlotsKinReco.h
@@ -16,18 +16,30 @@ namespace ztop{
     public:
     analysisPlotsKinReco(size_t step):analysisPlots(step),
             mtt(0),
+            mtt_coarse(0),
             pt_top(0),
             eta_top(0),
             pt_antitop(0),
             eta_antitop(0),
             pt_ttbar(0),
-            eta_ttbar(0)
+            eta_ttbar(0),
+            bin_1_(420),
+            bin_2_(550),
+            mlbcombthresh_(165)
             {
                 m_mub_3_.resize(cat_bjetmax,0);
                 m_mub_5_.resize(cat_bjetmax,0);
                 m_mub_7_.resize(cat_bjetmax,0);
                 m_mub_10_.resize(cat_bjetmax,0);
                 last_jet_pt_.resize(cat_bjetmax,0);
+
+                resizeVec(total_mtt_bjet_,cat_mttmax_reco,cat_bjetmax);
+                resizeVec(m_lb_3_mtt_bjet_,cat_mttmax_reco,cat_bjetmax);
+                resizeVec(m_mub_3_mtt_bjet_,cat_mttmax_reco,cat_bjetmax);
+                resizeVec(m_lb_5_mtt_bjet_,cat_mttmax_reco,cat_bjetmax);
+                resizeVec(m_mub_5_mtt_bjet_,cat_mttmax_reco,cat_bjetmax);
+                resizeVec(last_jet_pt_mtt_bjet_,cat_mttmax_reco,cat_bjetmax);
+
             }
   	~analysisPlotsKinReco(){ /* destruction is handled in base class! */
 	}
@@ -35,6 +47,11 @@ namespace ztop{
 	void bookPlots();
 	void fillPlotsReco();
 	void fillPlotsGen();
+
+        void resizeVec( std::vector<std::vector<histo1DUnfold*>> &vec , const size_t mtt_size , const size_t btag_size){
+            vec.resize(mtt_size);
+            for( auto &it : vec ) it.resize(btag_size,0);
+        }
 
     private:
 
@@ -48,21 +65,43 @@ namespace ztop{
             cat_0bjet, cat_1bjet, cat_2bjet, cat_bjetmax}
 	bjetcategory;
 
+	enum reco_mttcategories{
+            cat_mtt1_reco, cat_mtt2_reco, cat_mtt3_reco, cat_mttmax_reco}
+	reco_mttcategory;
+	enum gen_mttcategories{
+            cat_mtt1_gen, cat_mtt2_gen, cat_mtt3_gen, cat_mttmax_gen}
+	gen_mttcategory;
+
 	void setJetCategory(size_t nbjets,size_t njets);
 	void setBJetCategory(size_t nbjets);
+
+	void setGenMttCategory(float gen_mtt);
+	void setRecoMttCategory(float reco_mtt);
 
         std::vector<histo1DUnfold*> m_mub_3_, m_mub_5_, m_mub_7_, m_mub_10_;
         std::vector<histo1DUnfold*> last_jet_pt_;
 
+        std::vector<std::vector<histo1DUnfold*>> total_mtt_bjet_;
+        std::vector<std::vector<histo1DUnfold*>> m_lb_3_mtt_bjet_;
+        std::vector<std::vector<histo1DUnfold*>> m_mub_3_mtt_bjet_;
+        std::vector<std::vector<histo1DUnfold*>> m_lb_5_mtt_bjet_;
+        std::vector<std::vector<histo1DUnfold*>> m_mub_5_mtt_bjet_;
+        std::vector<std::vector<histo1DUnfold*>> last_jet_pt_mtt_bjet_;
 
         histo1DUnfold  
             * mtt,
+            * mtt_coarse,
             * pt_top,
             * eta_top,
             * pt_antitop,
             * eta_antitop,
             * pt_ttbar,
             * eta_ttbar;
+
+        float bin_1_; // 420 (mtt> 345 by construction)
+        float bin_2_; // 550
+	float mlbcombthresh_;
+
     };
 
 }
--- Analysis/src/analysisPlotsKinReco.cc
+++ Analysis/src/analysisPlotsKinReco.cc
@@ -56,6 +56,24 @@ namespace ztop{
 	else if(nbjets < 3)        bjetcategory=cat_2bjet;
     }
 
+    void analysisPlotsKinReco::setGenMttCategory(float gen_mtt){
+        if ((int)cat_mttmax_reco != (int)cat_mttmax_gen)
+            throw std::logic_error("analysisPlotsKinReco::setMttCategory: gen and reco mtt bins must be the same");
+
+        if (gen_mtt < bin_1_) gen_mttcategory = cat_mtt1_gen;
+        else if (gen_mtt < bin_2_) gen_mttcategory = cat_mtt2_gen;
+        else gen_mttcategory = cat_mtt3_gen;
+    }
+
+    void analysisPlotsKinReco::setRecoMttCategory(float reco_mtt){
+        if ((int)cat_mttmax_reco != (int)cat_mttmax_reco)
+            throw std::logic_error("analysisPlotsKinReco::setMttCategory: gen and reco mtt bins must be the same");
+
+        if (reco_mtt < bin_1_) reco_mttcategory = cat_mtt1_reco;
+        else if (reco_mtt < bin_2_) reco_mttcategory = cat_mtt2_reco;
+        else reco_mttcategory = cat_mtt3_reco;
+    }
+
     void analysisPlotsKinReco::bookPlots(){
 	if(!use()) return;
 	using namespace std;
@@ -68,9 +86,11 @@ namespace ztop{
 
         vector<float> eta_bins=histo1D::createBinning(60,-3.,3.);        
         vector<float> mtt_bins, pt_bins;
+        vector<float> mtt_bins_fine;
 
+	std::vector<float> nobins=histo1D::createBinning(1,0,1);
 	std::vector<float> bins=histo1D::createBinning(1,0,1); bins.clear();
-        bins << 30 << 35 << 40<< 45 << 50 << 55 << 60 << 70 << 80 << 90 << 100 << 120 << 140 << 200;
+        bins << 30 << 35 << 40<< 45 << 50 << 55 << 60 << 70 << 80 << 90 << 100 << 120 << 140 << 160 << 200;
 
 	for(float i=0;i<=700;i+=20){
             pt_bins<<i;	}	
@@ -79,7 +99,10 @@ namespace ztop{
         pt_bins<<2000;
         mtt_bins<<5000;
 
+        mtt_bins_fine << 300 << 420 << 550 << 2000 ;
+
         mtt=addPlot(mtt_bins,mtt_bins,"m_tt kin reco","m_{t#bar{t}} [GeV]", "Events/GeV");
+        mtt_coarse=addPlot(mtt_bins_fine,mtt_bins_fine,"m_tt kin reco coarse","m_{t#bar{t}} [GeV]", "Events/GeV");
         pt_top=addPlot(pt_bins,pt_bins,"top pt kin reco","top p_{T} [GeV]", "Events/GeV");
         eta_top=addPlot(eta_bins,eta_bins,"top eta kin reco","top #eta", "Events");
         pt_antitop=addPlot(pt_bins,pt_bins,"antitop pt kin reco","antitop p_{T} [GeV]", "Events/GeV");
@@ -88,11 +111,19 @@ namespace ztop{
         eta_ttbar=addPlot(eta_bins,eta_bins,"ttbar eta kin reco","t#bar{t} #eta", "Events");
 
 	for(size_t nbjet=0;nbjet<cat_bjetmax;nbjet++){
-            m_mub_3_.at(nbjet)=addPlot(verycoarsebins,verycoarsebins,"m_mub min very coarse "+toTString(nbjet)+" b-jets","m_{#mub} [GeV]", "Events/GeV");
-            m_mub_5_.at(nbjet)=addPlot(coarsebins,coarsebins,"m_mub min coarse "+toTString(nbjet)+" b-jets","m_{#mub} [GeV]", "Events/GeV");
-            m_mub_7_.at(nbjet)=addPlot(finebins,finebins,"m_mub min fine "+toTString(nbjet)+" b-jets","m_{#mub} [GeV]", "Events/GeV");
-            m_mub_10_.at(nbjet)=addPlot(veryfinebins,veryfinebins,"m_mub min very fine "+toTString(nbjet)+" b-jets","m_{#mub} [GeV]", "Events/GeV");
+            m_mub_3_.at(nbjet)=addPlot(verycoarsebins,verycoarsebins,"m_mub very coarse "+toTString(nbjet)+" b-jets","m_{#mub} [GeV]", "Events/GeV");
+            m_mub_5_.at(nbjet)=addPlot(coarsebins,coarsebins,"m_mub coarse "+toTString(nbjet)+" b-jets","m_{#mub} [GeV]", "Events/GeV");
+            m_mub_7_.at(nbjet)=addPlot(finebins,finebins,"m_mub fine "+toTString(nbjet)+" b-jets","m_{#mub} [GeV]", "Events/GeV");
+            m_mub_10_.at(nbjet)=addPlot(veryfinebins,veryfinebins,"m_mub very fine "+toTString(nbjet)+" b-jets","m_{#mub} [GeV]", "Events/GeV");
             last_jet_pt_.at(nbjet)=addPlot(bins,bins,"last jet pt "+toTString(nbjet)+" b-jets","p_{T} [GeV]","Events/GeV");
+            for(size_t mtt_cat=0; mtt_cat<cat_mttmax_reco; mtt_cat++){
+                total_mtt_bjet_.at(mtt_cat).at(nbjet)=addPlot(nobins,nobins,"total "+toTString(nbjet)+" b-jets mtt"+toTString((int)mtt_cat+1),"", "Event yield");
+                last_jet_pt_mtt_bjet_.at(mtt_cat).at(nbjet)=addPlot(bins,bins,"last jet pt "+toTString(nbjet)+" b-jets mtt"+toTString((int)mtt_cat+1),"p_{T} [GeV]", "Events/GeV");
+                m_mub_3_mtt_bjet_.at(mtt_cat).at(nbjet)=addPlot(verycoarsebins,verycoarsebins,"m_mub very coarse "+toTString(nbjet)+" b-jets mtt"+toTString((int)mtt_cat+1),"m_{#mub} [GeV]","Events/GeV");
+                m_mub_5_mtt_bjet_.at(mtt_cat).at(nbjet)=addPlot(coarsebins,coarsebins,"m_mub coarse "+toTString(nbjet)+" b-jets mtt"+toTString((int)mtt_cat+1),"m_{#mub} [GeV]", "Events/GeV");
+                m_lb_5_mtt_bjet_.at(mtt_cat).at(nbjet)=addPlot(coarsebins,coarsebins,"m_lb min coarse "+toTString(nbjet)+" b-jets mtt"+toTString((int)mtt_cat+1),"m_{lb}^{min} [GeV]", "Events/GeV");
+                m_lb_3_mtt_bjet_.at(mtt_cat).at(nbjet)=addPlot(verycoarsebins,verycoarsebins,"m_lb min very coarse "+toTString(nbjet)+" b-jets mtt"+toTString((int)mtt_cat+1),"m_{lb}^{min} [GeV]", "Events/GeV");            
+            }
         }
 
     }
@@ -103,9 +134,6 @@ namespace ztop{
 	if(!event()) return;
 	using namespace std;
 	using namespace ztop;
-
-        if (!event()->gentops) return;
-        if (event()->gentops->size()<2) return;
         
 	std::vector<NTGenParticle*> genvisleptons1=produceCollection(event()->genleptons1,20,2.4);
 	std::vector<NTGenJet*> genvisjets=produceCollection(event()->genjets,30,2.4);
@@ -114,9 +142,6 @@ namespace ztop{
         if(!requireNumber(2,genvisjets)) return;
         if(genvisleptons1.at(0)->pt()<25 && genvisleptons1.at(1)->pt()<25) return;
 
-        mtt->fillGen((event()->gentops->at(0)->p4()+event()->gentops->at(1)->p4()).m(),puweight());
-        pt_ttbar->fillGen((event()->gentops->at(0)->p4()+event()->gentops->at(1)->p4()).pt(),puweight());
-        eta_ttbar->fillGen((event()->gentops->at(0)->p4()+event()->gentops->at(1)->p4()).eta(),puweight());
 
         for(size_t i=0;i<cat_bjetmax;i++){
             m_mub_3_.at(i)->fillGen(20.5,puweight());
@@ -124,8 +149,24 @@ namespace ztop{
             m_mub_7_.at(i)->fillGen(20.5,puweight());
             m_mub_10_.at(i)->fillGen(20.5,puweight());
             last_jet_pt_.at(i)->fillGen(30.5,puweight());
+
+            for(size_t j=0;j<cat_mttmax_reco;j++){
+                total_mtt_bjet_.at(j).at(i)->fillGen(.5,puweight());
+                last_jet_pt_mtt_bjet_.at(j).at(i)->fillGen(30.5,puweight());
+                m_lb_3_mtt_bjet_.at(j).at(i)->fillGen(20.5,puweight());
+                m_lb_5_mtt_bjet_.at(j).at(i)->fillGen(20.5,puweight());
+                m_mub_3_mtt_bjet_.at(j).at(i)->fillGen(20.5,puweight());
+                m_mub_5_mtt_bjet_.at(j).at(i)->fillGen(20.5,puweight());
+            }
         }
 
+        if (!event()->gentops) return;
+        if (event()->gentops->size()<2) return;
+
+        mtt->fillGen((event()->gentops->at(0)->p4()+event()->gentops->at(1)->p4()).m(),puweight());
+        mtt_coarse->fillGen((event()->gentops->at(0)->p4()+event()->gentops->at(1)->p4()).m(),puweight());
+        pt_ttbar->fillGen((event()->gentops->at(0)->p4()+event()->gentops->at(1)->p4()).pt(),puweight());
+        eta_ttbar->fillGen((event()->gentops->at(0)->p4()+event()->gentops->at(1)->p4()).eta(),puweight());
 
         int itop = 0; int iantitop = 1;
         if(event()->gentops->at(0)->pdgId()<0){
@@ -135,7 +176,7 @@ namespace ztop{
         eta_top->fillGen(event()->gentops->at(itop)->eta(),puweight());
         pt_antitop->fillGen(event()->gentops->at(iantitop)->pt(),puweight());
         eta_antitop->fillGen(event()->gentops->at(iantitop)->eta(),puweight());
-
+ 
     }
 
     void analysisPlotsKinReco::fillPlotsReco(){
@@ -149,8 +190,10 @@ namespace ztop{
 		nbjets=event()->selectedbjets->size();
 		setBJetCategory(nbjets);
 	}
+        setRecoMttCategory(*event()->mtt);
 
         mtt->fillReco(*event()->mtt,puweight());
+        mtt_coarse->fillReco(*event()->mtt,puweight());
         pt_ttbar->fillReco(*event()->pt_ttbar,puweight());
         eta_ttbar->fillReco(*event()->eta_ttbar,puweight());
         pt_top->fillReco(*event()->pt_top,puweight());
@@ -164,8 +207,32 @@ namespace ztop{
         m_mub_10_.at(bjetcategory)->fillReco(*event()->m_mub,puweight());
 
         if (event()->selectedjets->size()>0){
-            last_jet_pt_.at(bjetcategory)->fillReco( event()->selectedjets->at(event()->selectedjets->size()-1)->pt() ,puweight());
+            float last_jet_pt = event()->selectedjets->at(event()->selectedjets->size()-1)->pt();
+            last_jet_pt_.at(bjetcategory)->fillReco( last_jet_pt , puweight());
+            last_jet_pt_mtt_bjet_.at(reco_mttcategory).at(bjetcategory)->fillReco( last_jet_pt , puweight());
         }
+
+        total_mtt_bjet_.at(reco_mttcategory).at(bjetcategory)->fillReco(.5,puweight());
+        m_mub_3_mtt_bjet_.at(reco_mttcategory).at(bjetcategory)->fillReco( *event()->m_mub , puweight());                
+        m_mub_5_mtt_bjet_.at(reco_mttcategory).at(bjetcategory)->fillReco( *event()->m_mub , puweight());                
+
+        if(event()->leadinglep && event()->secleadinglep && event()->selectedbjets ){
+            if(event()->selectedbjets->size()>0 ){
+                NTLorentzVector<float> bjetp4=event()->selectedbjets->at(0)->p4();
+                NTLorentzVector<float> llepp4=event()->leadinglep->p4();
+                NTLorentzVector<float> slepp4=event()->secleadinglep->p4();
+
+                float fMlb=(bjetp4+llepp4).m();
+                float fm2lb=(bjetp4+slepp4).m();
+                float fmlbmin=fMlb;
+                if(fMlb>fm2lb) fmlbmin=fm2lb;
+
+                m_lb_3_mtt_bjet_.at(reco_mttcategory).at(bjetcategory)->fillReco( fmlbmin , puweight());                
+                m_lb_5_mtt_bjet_.at(reco_mttcategory).at(bjetcategory)->fillReco( fmlbmin , puweight());                
+
+            }
+        }
+
     }
 
 }//ns
