be07f6f916c188903d77eb2ca7f653ecde0f61aa
defranchis <matteo.defranchis@cern.ch>
2019-08-16 12:25:40 +0200

    updated plot style

--- Analysis/pyscripts/makeXsecPlot.py
+++ Analysis/pyscripts/makeXsecPlot.py
@@ -1,11 +1,14 @@
 import ROOT
 from ROOT import TString, TFile, TGraph, TGraphAsymmErrors, TCanvas
 
+
+preliminary = False
+
 th_xsec_164 = [261.4, 304.4, 177.3, 46.6]
 th_xsec_162 = [294.1, 314.8, 181.0, 47.3]
 th_xsec_166 = [231.4, 294.7, 173.3, 46.2]
 
-bins = [345,420,550,810,2000]
+bins = [340,420,550,810,2000]
 scales = [384.0, 476.2, 644.3, 1023.6]
 
 rFile = TFile('toys.root')
@@ -69,7 +72,7 @@ for bin in range(1,5):
 
     xsec_err = getXsec(bin)
     g_exp.SetPoint(bin-1, scales[bin-1], xsec_err[0])
-    g_exp.SetPointError(bin-1,scales[bin-1]-bins[bin-1], bins[bin]-scales[bin-1], xsec_err[2], xsec_err[1])
+    g_exp.SetPointError(bin-1, 0, 0, xsec_err[2], xsec_err[1])
 
 
 g_th_164.SetMarkerStyle(0)
@@ -79,10 +82,12 @@ g_th_164.SetLineColor(ROOT.kRed)
 g_th_162.SetMarkerStyle(0)
 g_th_162.SetMarkerColor(ROOT.kBlue)
 g_th_162.SetLineColor(ROOT.kBlue)
+g_th_162.SetLineStyle(2)
 
 g_th_166.SetMarkerStyle(0)
 g_th_166.SetMarkerColor(ROOT.kGreen+2)
 g_th_166.SetLineColor(ROOT.kGreen+2)
+g_th_166.SetLineStyle(3)
 
 g_exp.SetMarkerStyle(8)
 g_exp.SetLineWidth(1)
@@ -97,34 +102,80 @@ latexLabel2.SetTextSize(0.04)
 latexLabel2.SetTextFont(42)
 latexLabel2.SetNDC()
 
-leg = ROOT.TLegend(.30,.65,.89,.85)
-leg.SetBorderSize(0)
-leg.AddEntry(g_exp,'unfolded differential cross section','ple')
-leg.AddEntry(g_th_162,'NLO prediction, m_{t}(m_{t})=162GeV, ABMP16','pl')
-leg.AddEntry(g_th_164,'NLO prediction, m_{t}(m_{t})=164GeV, ABMP16','pl')
-leg.AddEntry(g_th_166,'NLO prediction, m_{t}(m_{t})=166GeV, ABMP16','pl')
+latexLabel3 = ROOT.TLatex()
+latexLabel3.SetTextSize(0.04)
+latexLabel3.SetTextFont(42)
+latexLabel3.SetNDC()
+
+leg1 = ROOT.TLegend(.40,.75,.89,.85)
+leg1.SetBorderSize(0)
+leg1.AddEntry(g_exp,'data unfolded to parton level','pe')
+leg2 = ROOT.TLegend(.52,.33,.89,.48)
+leg2.SetBorderSize(0)
+leg2.AddEntry(g_th_162,'m_{t}(m_{t}) = 162 GeV','pl')
+leg2.AddEntry(g_th_164,'m_{t}(m_{t}) = 164 GeV','pl')
+leg2.AddEntry(g_th_166,'m_{t}(m_{t}) = 166 GeV','pl')
 
 c = TCanvas()
 c.SetLeftMargin(0.12)
 c.SetBottomMargin(0.12)
-g_exp.GetXaxis().SetTitle('m_{t#bar{t}} [GeV]')
-g_exp.GetYaxis().SetTitle('#frac{d#sigma}{dm_{t#bar{t}}} #Deltam_{t#bar{t}} [pb]')
-g_exp.GetYaxis().SetTitleOffset(1.1)
-g_exp.GetXaxis().SetTitleSize(0.048)
-g_exp.GetYaxis().SetTitleSize(0.045)
+g_th_162.GetXaxis().SetTitle('m_{t#bar{t}} [GeV]')
+g_th_162.GetYaxis().SetTitle('#frac{d#sigma}{dm_{t#bar{t}}} #Deltam_{t#bar{t}} [pb]')
+g_th_162.GetYaxis().SetTitleOffset(1.1)
+g_th_162.GetXaxis().SetTitleSize(0.048)
+g_th_162.GetYaxis().SetTitleSize(0.045)
 
+
+g_th_162.Draw('ap')
+g_exp.Draw('psame')
+g_th_164.Draw('psame')
+g_th_166.Draw('psame')
+g_exp.Draw('psame')
+leg1.Draw('same')
+leg2.Draw('same')
+
+
+latexLabel1.DrawLatex(0.12, 0.92, "CMS")
+latexLabel2.DrawLatex(0.70, 0.92, "35.9 fb^{-1} (13 TeV)")
+if preliminary: latexLabel3.DrawLatex(0.215, 0.92 , "#it{Preliminary}")
+
+latexLabel3.DrawLatex(.52, 0.67, "NLO predictions in #bar{MS} scheme")
+latexLabel3.DrawLatex(.52, 0.62, "#mu_{r} = #mu_{f} = m_{t}(m_{t})")
+latexLabel3.DrawLatex(.52, 0.54, "ABMP16_5_nlo PDF set")
+
+c.SaveAs('diffXsec.png')
+c.SaveAs('diffXsec.pdf')
+c.SaveAs('diffXsec.root')
+
+
+g_th_164.Clear()
+g_th_162.Clear()
+g_th_166.Clear()
+g_exp.Clear()
+
+for bin in range(1,5):
+    g_th_164.SetPoint(bin-1, scales[bin-1], th_xsec_164[bin-1]/(bins[bin]-bins[bin-1]))
+    g_th_164.SetPointError(bin-1,scales[bin-1]-bins[bin-1], bins[bin]-scales[bin-1], 0, 0)
+    g_th_162.SetPoint(bin-1, scales[bin-1], th_xsec_162[bin-1]/(bins[bin]-bins[bin-1]))
+    g_th_162.SetPointError(bin-1,scales[bin-1]-bins[bin-1], bins[bin]-scales[bin-1], 0, 0)
+    g_th_166.SetPoint(bin-1, scales[bin-1], th_xsec_166[bin-1]/(bins[bin]-bins[bin-1]))
+    g_th_166.SetPointError(bin-1,scales[bin-1]-bins[bin-1], bins[bin]-scales[bin-1], 0, 0)
+
+    xsec_err = getXsec(bin)
+    g_exp.SetPoint(bin-1, scales[bin-1], xsec_err[0]/(bins[bin]-bins[bin-1]))
+    g_exp.SetPointError(bin-1,scales[bin-1]-bins[bin-1], bins[bin]-scales[bin-1], xsec_err[2]/(bins[bin]-bins[bin-1]), xsec_err[1]/(bins[bin]-bins[bin-1]))
+
+c.Clear()
+c.SetLogy()
+
+g_exp.GetYaxis().SetTitle('#frac{d#sigma}{dm_{t#bar{t}}} [pb/GeV]')
 g_exp.Draw('ap')
 g_th_164.Draw('psame')
 g_th_162.Draw('psame')
 g_th_166.Draw('psame')
 g_exp.Draw('psame')
-leg.Draw('same')
-
-latexLabel1.DrawLatex(0.12, 0.92, "CMS")
-latexLabel2.DrawLatex(0.70, 0.92, "35.9 fb^{-1} (13 TeV)")
-
-
-
-c.SaveAs('diffXsec.png')
-c.SaveAs('diffXsec.pdf')
-c.SaveAs('diffXsec.root')
+leg1.Draw('same')
+leg2.Draw('same')
+c.SaveAs('diffXsec_norm.png')
+c.SaveAs('diffXsec_norm.pdf')
+c.SaveAs('diffXsec_norm.root')
